f00c5a0cc61390afbd85eb89aba8d478d84cd1d3
谢靖伦 <jinglun.xie@corp.elong.com>
2015-12-04 18:54:16 +0800

    add mobile sitemap

--- lib/tasks/sitemap.rb
+++ lib/tasks/sitemap.rb
@@ -1,8 +1,10 @@
 class Sitemap 
-  def initialize
+  def initialize(sitemap_type=nil)
+    @sitemap_type = sitemap_type
     @date = Time.now.strftime("%Y-%m-%d")
     @year = Time.now.strftime("%Y")
-    @file_name = "public/sitemap/"
+    @file_name_prefix = "public/sitemap/#{@sitemap_type}fishtrip_danti"
+    @index_file_name = "public/sitemap/sitemap#{@sitemap_type}_index.xml"
     @header = %Q(<?xml version="1.0" encoding="UTF-8" ?>\n <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n)
     @xml_path_list = []
   end
@@ -10,7 +12,7 @@ class Sitemap
   def work
     index=0
     Hotel.find_in_batches(batch_size: 5000) do |hotel_batch|
-      xml_file_name = "#{@file_name}fishtrip_danti_#{index}_#{@year}.xml"
+      xml_file_name = "#{@file_name_prefix}_#{index}_#{@year}.xml"
       write_items_to_file(hotel_batch, xml_file_name)
       index+=1
     end
@@ -21,7 +23,11 @@ class Sitemap
     xml_file = File.new(file_name,"w+")
     xml_file.write(@header)
     hotel_batch.each do |hotel|
-      url = "http://hotel.timlentse.com/#{hotel.fishtrip_hotel_id}/"
+      if @sitemap_type=='mobile'
+        url = "http://m.timlentse.com/#{hotel.fishtrip_hotel_id}/"
+      else
+        url = "http://hotel.timlentse.com/#{hotel.fishtrip_hotel_id}/"
+      end
       item=<<EOF
   <url>
     <loc>#{url}</loc>
@@ -38,7 +44,7 @@ EOF
   end
 
   def create_sitemap_index
-    sitemap_index = File.new("public/sitemap/sitemap_index.xml","w+")
+    sitemap_index = File.new(@index_file_name,"w+")
     sitemap_index.write(%Q(<?xml version="1.0" encoding="UTF-8"?>\n <sitemapindex>\n))
     @xml_path_list.each do |file|
       file = file.gsub(/public\/sitemap\//, '')
--- lib/tasks/tasks.rake
+++ lib/tasks/tasks.rake
@@ -5,3 +5,10 @@ task :update_sitemap=>:environment do
   sitemap_obj.work
   sitemap_obj.create_sitemap_index
 end
+
+desc "Task for creating sitemap mobile"
+task :update_sitemap_mobile=>:environment do
+  sitemap_obj = Sitemap.new
+  sitemap_obj.work
+  sitemap_obj.create_sitemap_index
+end
