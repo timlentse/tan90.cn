0cc5fff1212a2177c60c78959a080700d290ca47
谢靖伦 <jinglun.xie@corp.elong.com>
2015-12-12 18:28:25 +0800

    change creating sitemap logic

--- lib/tasks/sitemap.rb
+++ lib/tasks/sitemap.rb
@@ -1,65 +1,31 @@
 class Sitemap 
-  def initialize(sitemap_type=nil)
+  def initialize(sitemap_type)
     @sitemap_type = sitemap_type
     @date = Time.now.strftime("%Y-%m-%d")
     @year = Time.now.strftime("%Y")
-    @file_name_prefix = "public/sitemap/#{@sitemap_type}fishtrip_danti"
-    @index_file_name = "public/sitemap/sitemap#{@sitemap_type}_index.xml"
-    @header = %Q(<?xml version="1.0" encoding="UTF-8" ?>\n <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n)
     @xml_path_list = []
-    @base_url = @sitemap_type=='mobile' ? "http://m.timlentse.com" : "http://hotel.timlentse.com"
   end
 
-  def work
+  def create_sitemap(product, page_type)
+    @file_name_prefix = "public/sitemap/#{@sitemap_type}_#{product}_#{page_type}"
+    @template_file = "lib/tasks/#{@sitemap_type}_#{product}_#{page_type}.erb" 
     index=0
     Hotel.find_in_batches(batch_size: 5000) do |hotel_batch|
+      res = ERB.new(File.read(@template_file)).result(binding)
       xml_file_name = "#{@file_name_prefix}_#{index}_#{@year}.xml"
-      write_items_to_file(hotel_batch, xml_file_name)
-      index+=1
+      xml_file = File.open(xml_file_name,'w+')
+      xml_file.write(res)
+      @xml_path_list << xml_file_name
+      index += 1
     end
-    puts "Danti generated successfully "
-  end
-
-  def write_items_to_file(hotel_batch, file_name)
-    xml_file = File.new(file_name,"w+")
-    xml_file.write(@header)
-    hotel_batch.each do |hotel|
-      if @sitemap_type=='mobile'
-        url = "http://m.timlentse.com/#{hotel.fishtrip_hotel_id}/"
-      else
-        url = "http://hotel.timlentse.com/#{hotel.fishtrip_hotel_id}/"
-      end
-      item=<<EOF
-  <url>
-    <loc>#{url}</loc>
-    <lastmod>#{@date}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-EOF
-      xml_file.write(item)
-    end
-    xml_file.write("</urlset>\n")
-    xml_file.close
-    @xml_path_list << file_name
+    puts "Sitemap #{product} #{page_type} generated successfully "
   end
 
   def create_sitemap_index
-    sitemap_index = File.new(@index_file_name,"w+")
-    sitemap_index.write(%Q(<?xml version="1.0" encoding="UTF-8"?>\n <sitemapindex>\n))
-    @xml_path_list.each do |file|
-      file = file.gsub(/public\/sitemap\//, '')
-      sitemap_item=<<EOF
-  <sitemap> 
-    <loc>
-      #{@base_url}/sitemap/#{file}
-    </loc>
-  </sitemap>
-EOF
-      sitemap_index.write(sitemap_item)
-    end
-    sitemap_index.write("</sitemapindex>\n")
-    sitemap_index.close
+    @index_file_name = "public/sitemap/#{@sitemap_type}_sitemap_index.xml"
+    @template_index= "lib/tasks/#{@sitemap_type}_index.erb" 
+    sitemap_index_file = File.new(@index_file_name,"w+")
+    res = ERB.new(File.read(@template_index)).result(binding)
+    sitemap_index_file.write(res)
   end
-
 end
--- lib/tasks/tasks.rake
+++ lib/tasks/tasks.rake
@@ -1,14 +1,14 @@
 require_relative 'sitemap.rb'
 desc "Task for creating sitemap"
 task :update_sitemap=>:environment do
-  sitemap_obj = Sitemap.new
-  sitemap_obj.work
+  sitemap_obj = Sitemap.new('pc')
+  sitemap_obj.create_sitemap('fishtrip', 'danti')
   sitemap_obj.create_sitemap_index
 end
 
 desc "Task for creating sitemap mobile"
 task :update_sitemap_mobile=>:environment do
   sitemap_obj = Sitemap.new('mobile')
-  sitemap_obj.work
+  sitemap_obj.create_sitemap('fishtrip', 'danti')
   sitemap_obj.create_sitemap_index
 end
