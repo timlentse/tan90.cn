28aadcadf17e5f3a91c5b23d6c5e384e17709366
谢靖伦 <jinglun.xie@corp.elong.com>
2015-12-14 10:26:05 +0800

    fix wrong pagination

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -9,11 +9,11 @@ class Hotel < ActiveRecord::Base
   # Filter function
   def self.search(args={})
     @page_id = args[:page]
-    args.delete(:page)
+    conditions = args.reject{|k,v| k.to_s=='page'}
     if @page_id and @page_id.to_i>1
-      @hotels = self.where(args).offset((@page_id.to_i-1)*21).limit(21)
+      @hotels = self.where(conditions).offset((@page_id.to_i-1)*21).limit(21)
     else
-      @hotels = self.where(args).limit(21)
+      @hotels = self.where(conditions).limit(21)
     end
   end
 
