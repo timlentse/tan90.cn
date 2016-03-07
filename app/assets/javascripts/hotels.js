8d0bc461594e1edd1385325da3f42a4282579f8e
谢靖伦 <jinglun.xie@corp.elong.com>
2016-03-07 14:41:22 +0800

    use qiniu to serve static files

--- app/assets/javascripts/hotels.js
+++ app/assets/javascripts/hotels.js
@@ -1,4 +1,28 @@
 $(document).ready(function (){
+
+  // init checkin and checkout date %>
+  var start_day=new Date();
+  var end_day= new Date();
+  end_day.setDate(start_day.getDate() + 1);
+  $(function () { 
+    $("#checkin").datetimepicker({
+      format:'YYYY-MM-DD',
+      defaultDate:start_day
+    });
+
+    $("#checkout").datetimepicker({
+      format:'YYYY-MM-DD',
+      defaultDate:end_day
+    });
+    $('[data-toggle="popover"]').popover({"trigger": "hover"});
+  })
+
+  $("#search").click(function (e) {
+    e.preventDefault();
+    var location = $("#location").val() || '台北';
+    window.location.href="/fishtrip/search/?q="+location;
+  });
+
   //load more hotels
   $(".pager > li > a ").click(function (e) {
     e.preventDefault();
@@ -15,12 +39,6 @@ $(document).ready(function (){
         console.log(msg.responseText);
       }
     });
-  });
-
-  $("#search").click(function (e) {
-    e.preventDefault();
-    var location = $("#location").val() || '台北';
-    window.location.href="/fishtrip/search/?q="+location;
   });
 
 });
