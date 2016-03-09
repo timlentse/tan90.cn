5a8fed0aa2dfe9e83c47edd54a34ede68c95c74a
谢靖伦 <jinglun.xie@corp.elong.com>
2016-03-09 10:01:10 +0800

    迭代

--- app/controllers/commons_controller.rb
+++ app/controllers/commons_controller.rb
@@ -4,6 +4,10 @@ class CommonsController < ApplicationController
     @tdk = Common.tdk
     @booking_hot_destinations = Common.get_hot_destinations
     @fishtrip_links = Common.get_fishtrip_links
+    @cities = {}
+    ['taiwan','japan','thailand','korea'].each do |country|
+      @cities[country]=FishtripCity.where(country:country).take(3)
+    end
   end
 
 end
