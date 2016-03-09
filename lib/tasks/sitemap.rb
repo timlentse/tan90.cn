fd6858b0e02cd31bf3e23ccf7292e9b887b3be97
谢靖伦 <jinglun.xie@corp.elong.com>
2016-03-09 17:34:33 +0800

    add create sitemap for city page

--- lib/tasks/sitemap.rb
+++ lib/tasks/sitemap.rb
@@ -6,6 +6,19 @@ class Sitemap
   end
 
   def create_sitemap
+    @file_name_prefix = "public/sitemap/#{@sitemap_type}_fishtrip_city"
+    @template_file = "lib/tasks/#{@sitemap_type}_fishtrip_city.erb" 
+    index=0
+    FishtripCity.find_in_batches(batch_size: 5000) do |hotel_batch|
+      res = ERB.new(File.read(@template_file)).result(binding)
+      xml_file_name = "#{@file_name_prefix}_#{index}.xml"
+      xml_file = File.open(xml_file_name,'w+')
+      xml_file.write(res)
+      @xml_path_list << xml_file_name
+      index += 1
+    end
+    puts "Sitemap city generated successfully "
+
     @file_name_prefix = "public/sitemap/#{@sitemap_type}_fishtrip_danti"
     @template_file = "lib/tasks/#{@sitemap_type}_fishtrip_danti.erb" 
     index=0
