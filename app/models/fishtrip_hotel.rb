cb3a63846aea29883dc44d73796147b66042491c
timlentse <tinglenxan@gmail.com>
2016-04-03 14:59:53 +0800

    order hotel by page id

--- app/models/fishtrip_hotel.rb
+++ app/models/fishtrip_hotel.rb
@@ -11,9 +11,9 @@ class FishtripHotel < ActiveRecord::Base
     @page_id = args[:page]
     conditions = args.reject{|k,v| k.to_s=='page'}
     if @page_id and @page_id.to_i>1
-      @hotels = self.where(conditions).offset((@page_id.to_i-1)*21).limit(21)
+      @hotels = self.where(conditions).offset((@page_id.to_i-1)*21).order(:page_id).limit(21)
     else
-      @hotels = self.where(conditions).limit(21)
+      @hotels = self.where(conditions).order(:page_id).limit(21)
     end
   end
 
