e61fa213738236bcd21be088241f18e9dbca7940
timlentse <tinglenxan@gmail.com>
2016-09-23 16:58:23 +0800

    style modified

--- app/models/common.rb
+++ app/models/common.rb
@@ -11,7 +11,7 @@ class Common
   def self.get_hot_destinations
     mapper = ['热门区域','热门城市','热门地标']
     slinks=[]
-    BookingHotDestination.where(:cate=>1).find_each do |ds|
+    BookingHotDestination.where(:cate=>1,:is_show=>1).find_each do |ds|
       slinks[0].nil? ? slinks[0] = {:title=>'热门城市',:links=>[{:text=>ds.name_cn,:link=>"/booking/#{ds.cc}/#{ds.name_en}/",:addition=>ds.number_of_hotel}]} : slinks[0][:links].push({:text=>ds.name_cn,:link=>"/booking/#{ds.cc}/#{ds.name_en}/",:addition=>ds.number_of_hotel})
     end
     slinks
